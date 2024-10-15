5789bcaadec3513634433a264bc3589c00d6c5cc
Rampant098 <122899668+AdamKenny098@users.noreply.github.com>
2024-10-15 10:41:00 +0100

    Step 4

--- movies/src/components/movieCard/index.js
+++ movies/src/components/movieCard/index.js
@@ -1,4 +1,3 @@
-import React from "react";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
@@ -12,6 +11,7 @@ import StarRateIcon from "@mui/icons-material/StarRate";
 import IconButton from "@mui/material/IconButton";
 import Grid from "@mui/material/Grid2";
 import img from '../../images/film-poster-placeholder.png'
+import { Link } from "react-router-dom";
 
 export default function MovieCard(props) {
   const movie = props.movie;
@@ -46,9 +46,11 @@ export default function MovieCard(props) {
         <IconButton aria-label="add to favorites" onClick={null}>
           <FavoriteIcon color="primary" fontSize="large" />
         </IconButton>
-        <Button variant="outlined" size="medium" color="primary">
-          More Info ...
-        </Button>
+        <Link to={`/movies/${movie.id}`}>
+          <Button variant="outlined" size="medium" color="primary">
+            More Info ...
+          </Button>
+        </Link>
       </CardActions>
     </Card>
   );
